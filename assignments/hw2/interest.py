d0d312e36c5ea5009d7f5c7c94142901268a2b3e
Madison Byrne <byrneme@g.cofc.edu>
2021-09-15 15:02:10 -0400

    completed hw 3

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -1,3 +1,4 @@
+
 """
 Name: <Madison Byrne>
 <interest>.py
@@ -8,17 +9,21 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work
 """
 def main():
-    rate =eval(input("Enter a value for the interest rate:"))
-    days = eval(input("Enter a value for the amount of days in the billing cycle:"))
-    principle = eval(input("Enter a value for the previous net balance:"))
-    payment = eval(input("Enter a value for the payment amount:"))
+    interest_rate = eval(input("Enter a value for the annual interest rate:"))
+    billing_cycle = eval(input("Enter a value for the number in the billing cycle:"))
+    net_balance = eval(input("Enter a value for the previous net balance:"))
+    payment = eval(input("Enter a value for the payment made on the billing day:"))
     billing_day = eval(input("Enter a value for the day the payment was made:"))
-    step_1 =(principle * days)
-    step_2 =(payment * 11)
-    step_3 = (step_2 - step_1)
-    step_4 =(step_3/ billing_day)
-    print (step_4*(rate/12))
+    step_1 = (net_balance * billing_cycle)
+    step_2 = (payment * (billing_cycle - billing_day))
+    step_3 = (step_1-step_2)
+    average_daily_balance = (step_3/ billing_cycle)
+    monthly_interest_rate = (interest_rate/12)/100
+    interest_charge = average_daily_balance * monthly_interest_rate
+    print(round(interest_charge, 2))
 
+#'rate': 15.84, 'days': 31.0, 'previous_balance': 850.0, 'payment': 400.0, 'payment_day': 20.0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     main()
